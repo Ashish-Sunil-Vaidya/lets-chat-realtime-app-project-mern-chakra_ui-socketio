ca419c5924897f9d20bcb3ae235f9899bb96dd2f
Ashish Vaidya <ashuvaidya2003@gmail.com>
2024-07-03 16:04:21 +0530

    chore: Update proxy and CORS origin for development and production environments

--- backend/index.js
+++ backend/index.js
@@ -61,7 +61,7 @@ const server = app.listen(process.env.PORT, () => {
 const io = new Server(server, {
     pingTimeout: 60000,
     cors: {
-        origin: 'http://localhost:5173'
+        origin: '*',
     }
 });
 
